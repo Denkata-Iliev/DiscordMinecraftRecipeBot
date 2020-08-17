5e61259f93f6aaa409f5d862ffa9acd845085e03
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2020-08-18 00:58:32 +0300

    My first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+package main.java;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hi to you, mr. World!");
     }
 }
