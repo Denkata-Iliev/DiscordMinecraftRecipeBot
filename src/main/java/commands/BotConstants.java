2aa6431105ca08dc6627b7d309b19231b38d4c19
Denkata-Iliev <denislav9711@gmail.com>
2020-08-20 16:12:46 +0300

    special cases and some refactoring

--- src/main/java/commands/BotConstants.java
+++ src/main/java/commands/BotConstants.java
@@ -4,4 +4,17 @@ public class BotConstants {
 
     public static final String BASE_URL = "https://www.minecraftcrafting.info/";
     public static final String TEMP_PREFIX = "pr";
+    public static final String WOOD_URL = "imgs/craft_wood2.png";
+    public static final String STICKS_URL = "imgs/craft_stick.png";
+    public static final String WOODEN_PLANKS_URL = "imgs/craft_wood.gif";
+    public static final String DOORS_URL = "imgs/craft_doors.gif";
+    public static final String PICKAXES_URL = "imgs/craft_pickaxes.gif";
+    public static final String HOES_URL = "imgs/craft_hoes.gif";
+    public static final String SHOVELS_URL = "imgs/craft_shovels.gif";
+    public static final String AXES_URL = "imgs/craft_axes.gif";
+    public static final String HELMETS_URL = "imgs/craft_helmets.gif";
+    public static final String CHESTPLATES_URL = "imgs/craft_chestplates.gif";
+    public static final String LEGGINGS_URL = "imgs/craft_leggings.gif";
+    public static final String BOOTS_URL = "imgs/craft_boots.gif";
+    public static final String SWORDS_URL = "imgs/craft_swords.gif";
 }
--- src/main/java/commands/RecipeCommand.java
+++ src/main/java/commands/RecipeCommand.java
@@ -15,23 +15,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static commands.BotConstants.BASE_URL;
-import static commands.BotConstants.TEMP_PREFIX;
+import static commands.BotConstants.*;
 
 public class RecipeCommand extends ListenerAdapter {
 
     private static final String RECIPE_MESSAGE = "You need to specify the item you're looking for";
-    public static final String RECIPE = "Recipe";
+    private static final String RECIPE = "Recipe";
 
     @Override
     public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
         List<String> message = Arrays.asList(event.getMessage().getContentRaw().split(" "));
+        List<String> imgURLs = getImgURLs(BotConstants.BASE_URL);
 
         if (!event.getAuthor().isBot()) {
             if (message.get(0).equalsIgnoreCase(TEMP_PREFIX) && message.get(1).equalsIgnoreCase(RECIPE)) {
                 String searchedRecipe = getSearchedRecipe(message).toLowerCase();
+                if (searchedRecipe.contains("block")) {
+                    event.getChannel().sendMessage("You'll have to be a bit more specific").queue();
+                    return;
+                }
+                if (message.size() <= 2) {
+                    event.getChannel().sendMessage(RECIPE_MESSAGE).queue();
+                    return;
+                }
                 String title = getTitle(message);
-                List<String> imgURLs = getImgURLs(BotConstants.BASE_URL);
+                specialCases(searchedRecipe, title, event);
                 for (String imgUrl : imgURLs) {
                     if (imgUrl.contains(searchedRecipe)) {
                         sendCraftingRecipe(event, imgUrl, title);
@@ -42,12 +50,59 @@ public class RecipeCommand extends ListenerAdapter {
         }
     }
 
+    private void specialCases(String searchedRecipe, String title, GuildMessageReceivedEvent event) {
+        if (searchedRecipe.equalsIgnoreCase("wood")) {
+            sendCraftingRecipe(event, WOOD_URL, title);
+        }
+        if (searchedRecipe.equalsIgnoreCase("sticks")) {
+            sendCraftingRecipe(event, STICKS_URL, title);
+        }
+        if (searchedRecipe.contains("planks")) {
+            sendCraftingRecipe(event, WOODEN_PLANKS_URL, title);
+        }
+        if (searchedRecipe.contains("woodendoor") || searchedRecipe.contains("irondoor")) {
+            sendCraftingRecipe(event, DOORS_URL, title);
+        }
+        if (searchedRecipe.contains("pickaxe")) {
+            sendCraftingRecipe(event, PICKAXES_URL, title);
+        }
+        if (searchedRecipe.contains("hoes")) {
+            sendCraftingRecipe(event, HOES_URL, title);
+        }
+        if (searchedRecipe.contains("SHOVELS")) {
+            sendCraftingRecipe(event, SHOVELS_URL, title);
+        }
+        if (searchedRecipe.equalsIgnoreCase("woodenaxe")
+                || searchedRecipe.equalsIgnoreCase("stoneaxe")
+                || searchedRecipe.equalsIgnoreCase("ironaxe")
+                || searchedRecipe.equalsIgnoreCase("goldenaxe")
+                || searchedRecipe.equalsIgnoreCase("diamondaxe")
+                || searchedRecipe.equalsIgnoreCase("netheriteaxe")) {
+            sendCraftingRecipe(event, AXES_URL, title);
+        }
+        if (searchedRecipe.contains("sword")) {
+            sendCraftingRecipe(event, SWORDS_URL, title);
+        }
+        if (searchedRecipe.contains("helmet")) {
+            sendCraftingRecipe(event, HELMETS_URL, title);
+        }
+        if (searchedRecipe.contains("chestplate")) {
+            sendCraftingRecipe(event, CHESTPLATES_URL, title);
+        }
+        if (searchedRecipe.contains("leggings")) {
+            sendCraftingRecipe(event, LEGGINGS_URL, title);
+        }
+        if (searchedRecipe.contains("boots")) {
+            sendCraftingRecipe(event, BOOTS_URL, title);
+        }
+    }
+
     private String getTitle(List<String> message) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 2; i < message.size(); i++) {
+        for (int i = 2; i < message.size(); i++) {
             sb.append(message.get(i)).append(" ");
         }
-        return sb.toString();
+        return sb.toString().toUpperCase();
     }
 
     private void sendCraftingRecipe(GuildMessageReceivedEvent event, String imgUrl, String title) {
