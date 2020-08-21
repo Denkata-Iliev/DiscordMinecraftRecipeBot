3d083ec710f4fbfc431542f80ed6db62ef82ca91
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2020-08-21 21:45:18 +0300

    Added supporting classes and the bot can play music

--- src/main/java/commands/MusicCommand.java
+++ src/main/java/commands/MusicCommand.java
@@ -1,5 +1,6 @@
 package commands;
 
+import music.PlayerManager;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -17,6 +18,7 @@ public class MusicCommand extends ListenerAdapter {
         if(message.get(0).equalsIgnoreCase(BotConstants.TEMP_PREFIX)){
             AudioManager audioManager = event.getGuild().getAudioManager();
             GuildVoiceState memberVoiceState = event.getMember().getVoiceState();
+            PlayerManager manager = PlayerManager.getInstance();
             switch (message.get(1).toLowerCase()){
                 case "join":
                     audioManager.openAudioConnection(memberVoiceState.getChannel());
@@ -26,6 +28,9 @@ public class MusicCommand extends ListenerAdapter {
                     audioManager.closeAudioConnection();
                     event.getChannel().sendMessage("Leaving...").queue();
                     break;
+                case "play":
+                    manager.loadAndPlay(event.getChannel(), "https://www.youtube.com/watch?v=A8pOVirjGF0");
+                    manager.getGuildMusicManager(event.getGuild()).player.setVolume(10);
             }
         }
     }
